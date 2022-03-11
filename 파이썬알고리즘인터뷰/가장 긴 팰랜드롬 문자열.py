67179ef6d24228b785ccd83a966a6a7261ef6095
bong7233 <batmantwo7233@gmail.com>
2022-03-11 17:21:06 +0900

    팰린드롬 문자열

--- 파이썬알고리즘인터뷰/가장 긴 팰랜드롬 문자열.py
+++ 파이썬알고리즘인터뷰/가장 긴 팰랜드롬 문자열.py
@@ -1,14 +1,19 @@
-# 팰린드롬 구현
+# 가장 긴 팰린드롬 문자열 찾기
 def longPalindrome(s: str) -> str:
-    # 투포인터 구현
+
+    # 투포인터 구현함수
     def expand(left: int, right: int) -> str:
+
         # 두개의 포인터가 범위내에있고, 양끝이 같은문자를 나타내는지(대칭인지)확인
         while left >= 0 and right < len(s) and s[left] == s[right]:
+
             # 대칭이 계속 유지된다면, 양끝의 위치를 한칸씩 넓힘
             left -= 1
             right += 1
+
         # 대칭되는 양 끝단의 위치를 찾아서 슬라이싱후 리턴
         return s[left + 1:right]
+
 
     # 만약 문자열이 한글자이거나 이미 거꾸로해도 같은상황이라면 바로 문자열 리턴
     if len(s) < 2 or s == s[::-1]:
