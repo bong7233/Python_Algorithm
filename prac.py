2dde1541557aa3491cb76a30e6702ac4c3e34763
bong7233 <batmantwo7233@gmail.com>
2022-03-17 14:59:42 +0900

    Docs

--- prac.py
+++ prac.py
@@ -1,72 +1,21 @@
-class Node:
-    def __init__(self, item):
-        self.data = item
-        self.next = None
+import collections
 
-class LinkedList:
+progress = [55,30,93]
+speed = [5,30,1]
 
-    def __init__(self):
-        self.nodeCount = 0
-        self.head = Node(None)
-        self.tail = None
-        self.head.next = self.tail
+suc = list(zip(progress,speed))
+# print(suc)
 
-    def traverse(self):
-        result = []
-        curr = self.head
-        while curr.next:
-            curr = curr.next
-            result.append(curr.data)
-        return result
+n = 1
+dic = collections.defaultdict(int)
+# print(dic)
+while len(suc) != 0:
+    count = 0
+    if suc[-1][0] + suc[-1][1]*n >= 100:
+        suc.pop()
+        count += 1
+        dic[n] += 1
+    else:
+        n += 1
 
-    def getAt(self, pos):
-        if pos < 0 or pos > self.nodeCount:
-            return None
-
-        i = 0
-        curr = self.head
-        while i < pos:
-            curr = curr.next
-            i += 1
-
-        return curr
-
-    def insertAfter(self, prev, newNode):
-        newNode.next = prev.next
-        if prev.next is None:
-            self.tail = newNode
-        prev.next = newNode
-        self.nodeCount += 1
-        return True
-
-    def insertAt(self, pos, newNode):
-        if pos < 1 or pos > self.nodeCount + 1:
-            return False
-
-        if pos != 1 and pos == self.nodeCount + 1:
-            prev = self.tail
-        else:
-            prev = self.getAt(pos - 1)
-        return self.insertAfter(prev, newNode)
-
-    def popAfter(self, prev):
-		curr = prev.next
-		if prev.next == self.tail:
-			prev.next == None
-			self.tail = prev
-		else:
-			prev.next = curr.next
-		self.nodeCount -= 1
-		return curr.data
-
-
-    def popAt(self, pos):
-		if pos < 1 or pos > self.nodeCount:
-			raise IndexError
-
-		prev = self.getAt(pos-1)
-		return self.popAfter(prev)
-
-
-def solution(x):
-    return 0
+print(list(dic.values()))
---
+++ 프로그래머스/다리를 지나는 트럭.py
@@ -0,0 +1,26 @@
+
+def solution(bridge_length, weight, truck_weights):
+    count = 0   # 경과 시간
+    weight_sum = 0     # 다리위의 트럭들의 무게의 함
+    bridge = [] # 다리위에 있는 트럭들
+
+    while truck_weights:
+        count += 1
+
+        # 트럭이 다리위로 올라갈 수 있는 조건
+        # (다리위에 올라갈 수 있는 한도랑 같아도 뒤에서 하나씩 빼주기 때문에 상관없다)
+        if ((weight_sum + truck_weights[0]) <= weight and len(bridge) <= bridge_length):
+            bridge.append(truck_weights.pop(0)) # 트럭을 다리위에 올리고
+            weight_sum += bridge[-1] # 올린 트럭의 무게를 총 무게에 더한다
+        else:
+            # 다리위로 올라갈 수 없다면 빈 공간으로 채운다
+            # (bridge.pop(0)을 했을 때 경과 시간이 지나지도 않았는데 다리에 들어오자마자 나가는걸 막기 위해)
+            bridge.append(0)
+
+        # 경과한 시간이 다리 길이보다 크면, 다리에서 나온 트럭들의 무게를 빼준다
+        # (배열의 크기가 다리의 길이만큼 되도록 기다림, 첫 트럭 빠져나올 때 까지 기다리는 코드)
+        if count >= bridge_length:
+            weight_sum -= bridge.pop(0)
+
+    # 총 경과 시간 + 다리위에 있는 트럭들이 지나가는데 소요 할 시간
+    return count + bridge_length
