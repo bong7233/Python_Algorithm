a574a8d9fe31136d38964d4e8ec2a385f996a55c
bong7233 <batmantwo7233@gmail.com>
2022-03-24 00:23:45 +0900

    Docs

--- prac.py
+++ prac.py
@@ -1,102 +1,145 @@
-class ArrayStack:
+class Node:
+
+    def __init__(self, key, data):
+        self.key = key
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+    def insert(self, key, data):
+        if key < self.key:
+            if self.left:
+                self.left.insert(key, data)
+            else:
+                self.left = Node(key, data)
+        elif key > self.key:
+            if self.right:
+                self.right.insert(key, data)
+            else:
+                self.right = Node(key, data)
+        else:
+            raise KeyError('Key %s already exists.' % key)
+
+
+    def lookup(self, key, parent=None):
+        if key < self.key:
+            if self.left:
+                return self.left.lookup(key, self)
+            else:
+                return None, None
+        elif key > self.key:
+            if self.right:
+                return self.right.lookup(key, self)
+            else:
+                return None, None
+        else:
+            return self, parent
+
+
+    def inorder(self):
+        traversal = []
+        if self.left:
+            traversal += self.left.inorder()
+        traversal.append(self)
+        if self.right:
+            traversal += self.right.inorder()
+        return traversal
+
+
+    def countChildren(self):
+        count = 0
+        if self.left:
+            count += 1
+        if self.right:
+            count += 1
+        return count
+
+
+class BinSearchTree:
 
     def __init__(self):
-        self.data = []
-
-    def size(self):
-        return len(self.data)
-
-    def isEmpty(self):
-        return self.size() == 0
-
-    def push(self, item):
-        self.data.append(item)
-
-    def pop(self):
-        return self.data.pop()
-
-    def peek(self):
-        return self.data[-1]
+        self.root = None
 
 
-def splitTokens(exprStr):
-    tokens = []
-    val = 0
-    valProcessing = False
-    for c in exprStr:
-        if c == ' ':
-            continue
-        if c in '0123456789':
-            val = val * 10 + int(c)
-            valProcessing = True
+    def insert(self, key, data):
+        if self.root:
+            self.root.insert(key, data)
         else:
-            if valProcessing:
-                tokens.append(val)
-                val = 0
-            valProcessing = False
-            tokens.append(c)
-    if valProcessing:
-        tokens.append(val)
-
-    return tokens
+            self.root = Node(key, data)
 
 
-def infixToPostfix(tokenList):
-    prec = {
-        '*': 3,
-        '/': 3,
-        '+': 2,
-        '-': 2,
-        '(': 1,
-    }
+    def lookup(self, key):
+        if self.root:
+            return self.root.lookup(key)
+        else:
+            return None, None
 
-    opStack = ArrayStack()
-    postfixList = []
 
-    for i in tokenList:
-        # 연산자이면서 Stack 이 비어있을 때
-        if i in prec and opStack.isEmpty():
-            opStack.push(i)
-
-        # 여는 괄호일 때 push
-        elif i == '(':
-            opStack.push(i)
-
-        elif i in prec:
-
-            # 연산자이면서 top 의 우선순위가 낮을 때 까지 pop
-            while not opStack.isEmpty():
-
-                if prec[opStack.peek()] >= prec[i]:
-                    postfixList.append(opStack.pop())
+    def remove(self, key):
+        node, parent = self.lookup(key)
+        if node:
+            nChildren = node.countChildren()
+            # The simplest case of no children
+            if nChildren == 0:
+                # 만약 parent 가 있으면
+                # node 가 왼쪽 자식인지 오른쪽 자식인지 판단하여
+                # parent.left 또는 parent.right 를 None 으로 하여
+                # leaf node 였던 자식을 트리에서 끊어내어 없앱니다.
+                if parent:
+                    pass
+                # 만약 parent 가 없으면 (node 는 root 인 경우)
+                # self.root 를 None 으로 하여 빈 트리로 만듭니다.
                 else:
-                    break
-            opStack.push(i)
+                    pass
+            # When the node has only one child
+            elif nChildren == 1:
+                # 하나 있는 자식이 왼쪽인지 오른쪽인지를 판단하여
+                # 그 자식을 어떤 변수가 가리키도록 합니다.
+                if node.left:
+                    pass
+                else:
+                    pass
+                # 만약 parent 가 있으면
+                # node 가 왼쪽 자식인지 오른쪽 자식인지 판단하여
+                # 위에서 가리킨 자식을 대신 node 의 자리에 넣습니다.
+                if parent:
+                    pass
+                # 만약 parent 가 없으면 (node 는 root 인 경우)
+                # self.root 에 위에서 가리킨 자식을 대신 넣습니다.
+                else:
+                    pass
+            # When the node has both left and right children
+            else:
+                parent = node
+                successor = node.right
+                # parent 는 node 를 가리키고 있고,
+                # successor 는 node 의 오른쪽 자식을 가리키고 있으므로
+                # successor 로부터 왼쪽 자식의 링크를 반복하여 따라감으로써
+                # 순환문이 종료할 때 successor 는 바로 다음 키를 가진 노드를,
+                # 그리고 parent 는 그 노드의 부모 노드를 가리키도록 찾아냅니다.
+                while successor.left:
+                    pass
+                # 삭제하려는 노드인 node 에 successor 의 key 와 data 를 대입합니다.
+                node.key = successor.key
+                node.data = successor.data
+                # 이제, successor 가 parent 의 왼쪽 자식인지 오른쪽 자식인지를 판단하여
+                # 그에 따라 parent.left 또는 parent.right 를
+                # successor 가 가지고 있던 (없을 수도 있지만) 자식을 가리키도록 합니다.
+                pass
 
-        # 닫는 괄호일 때 여는 괄호를 만날 때 까지 pop
-        elif i == ')':
-            while True:
-                t = opStack.pop()
-                if t == '(' or opStack.isEmpty():
-                    break
-                postfixList.append(t)
-        # 피연산자가 아닐 경우
+            return True
+
         else:
-            postfixList.append(i)
-
-        # 끝에 왔을 때 Stack에 남아있는 것 출력
-    while not opStack.isEmpty():
-        postfixList.append(opStack.pop())
-
-    return postfixList
+            return False
 
 
-def postfixEval(tokenList):
-    pass
+    def inorder(self):
+        if self.root:
+            return self.root.inorder()
+        else:
+            return []
 
 
-def solution(expr):
-    tokens = splitTokens(expr)
-    postfix = infixToPostfix(tokens)
-    val = postfixEval(postfix)
-    return val
+def solution(x):
+    return 0
