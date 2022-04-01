be1ad300e6dfb8ac719bd9c73d55fa4560a02a9d
bong7233 <batmantwo7233@gmail.com>
2022-04-01 12:28:51 +0900

    Docs

--- prac.py
+++ prac.py
@@ -1,51 +1,37 @@
-class BinaryMinHeap:
-    def __init__(self):
-        # 계산 편의를 위해 0이 아닌 1번째 인덱스부터 사용한다.
-        self.items = [None]
+from collections import defaultdict
+import re
 
-    def __len__(self):
-        # len() 연산을 가능하게 하는 매직 메서드 덮어쓰기(Override).
-        return len(self.items) - 1
+def solution(files):
 
-    def _percolate_up(self):
-        # percolate: 스며들다.
-        cur = len(self)
-        # left 라면 2*cur, right 라면 2*cur + 1 이므로 parent 는 항상 cur // 2
-        parent = cur // 2
+    if len(files) == 0:
+        return []
+    head = re.compile('[a-zA-Z]+')
+    number = re.compile('[0-9]{1,5}')
 
-        while parent > 0:
-            if self.items[cur] < self.items[parent]:
-                self.items[cur], self.items[parent] = self.items[parent], self.items[cur]
+    file_dic = defaultdict(list)
 
-            cur = parent
-            parent = cur // 2
+    for file in files:
+        file_head = head.search(file).group()
+        file_num = number.search(file).group()
 
-    def _percolate_down(self, cur):
-        smallest = cur
-        left = 2 * cur
-        right = 2 * cur + 1
+        file_dic[file].append(file_head.lower())
+        file_dic[file].append(int(file_num))
 
-        if left <= len(self) and self.items[left] < self.items[smallest]:
-            smallest = left
+    file_sorted = sorted(file_dic.items(), key=lambda x : (x[1][0], x[1][1]))
+    print(file_sorted)
+    answer = []
+    for file in file_sorted:
+        answer.append(file[0])
 
-        if right <= len(self) and self.items[right] < self.items[smallest]:
-            smallest = right
+    print(answer)
 
-        if smallest != cur:
-            self.items[cur], self.items[smallest] = self.items[smallest], self.items[cur]
-            self._percolate_down(smallest)
+li = ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]
+# 출력: ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
 
-    def insert(self, k):
-        self.items.append(k)
-        self._percolate_up()
+solution(li)
 
-    def extract(self):
-        if len(self) < 1:
-            return None
 
-        root = self.items[1]
-        self.items[1] = self.items[-1]
-        self.items.pop()
-        self._percolate_down(1)
+s= "img12asg.png"
 
-        return root
+t = re.split(r"([0-9]+)",s)
+print(t)
