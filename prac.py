f5419564a0369d05f395cb97b5405e4dba113b60
bong7233 <batmantwo7233@gmail.com>
2022-04-04 12:49:35 +0900

    Docs

--- prac.py
+++ prac.py
@@ -1,37 +1,9 @@
-from collections import defaultdict
-import re
+import heapq
 
-def solution(files):
+q = []
 
-    if len(files) == 0:
-        return []
-    head = re.compile('[a-zA-Z]+')
-    number = re.compile('[0-9]{1,5}')
+start = 5
 
-    file_dic = defaultdict(list)
+heapq.heappush(q, (0, start))
 
-    for file in files:
-        file_head = head.search(file).group()
-        file_num = number.search(file).group()
-
-        file_dic[file].append(file_head.lower())
-        file_dic[file].append(int(file_num))
-
-    file_sorted = sorted(file_dic.items(), key=lambda x : (x[1][0], x[1][1]))
-    print(file_sorted)
-    answer = []
-    for file in file_sorted:
-        answer.append(file[0])
-
-    print(answer)
-
-li = ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]
-# 출력: ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
-
-solution(li)
-
-
-s= "img12asg.png"
-
-t = re.split(r"([0-9]+)",s)
-print(t)
+print(q)
